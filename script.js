5b33add4349fa17573f65008fd8bca0245f7cc6f
poizoni <wkphillips11@gmail.com>
2022-04-13 15:57:13 -0400

    home page stylized

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const timer = document.getElementById('countdown');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const mins = document.getElementById('mins');
+const secs = document.getElementById('secs');
+
 
 let countDownDate = new Date('April 25, 2022 12:50').getTime();
 
@@ -7,15 +11,37 @@ let x = setInterval(() => {
 
     let distance = countDownDate - now;
 
-    let days = Math.floor(distance / (1000*60*60*24));
-    let hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-    let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-    let seconds = Math.floor((distance % (1000*60)) / 1000);
+    let calcDays = Math.floor(distance / (1000*60*60*24));
+    let calcHours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    let calcMins = Math.floor((distance % (1000*60*60)) / (1000*60));
+    let calcSecs = Math.floor((distance % (1000*60)) / 1000);
 
-    timer.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+    days.innerHTML = calcDays + 'd';
+    hours.innerHTML = calcHours + 'h';
+    mins.innerHTML = calcMins + 'm';
+    secs.innerHTML = calcSecs + 's';
 
     if(distance<0) {
         clearInterval(x);
         timer.innerHTML = 'expired';
     }
 }, 1000);
+
+
+let btn1 = document.getElementById('info');
+let btn2 = document.getElementById('score');
+let btn3 = document.getElementById('map');
+let btn4 = document.getElementById('rules');
+
+btn1.onclick = () => {
+    location.href = 'details.html'
+};
+btn2.onclick = () => {
+    location.href = 'score.html'
+};
+btn3.onclick = () => {
+    location.href = 'map.html'
+};
+btn4.onclick = () => {
+    location.href = 'rules.html'
+};
